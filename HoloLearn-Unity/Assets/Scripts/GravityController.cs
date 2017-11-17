d468b46177a006e6f6f79251b8750b22283fe95a
Emanuele Torelli <mennetorelli@hotmail.it>
2017-11-17 12:04:11 +0100

    disabilitata gravità durante dragging

---
+++ HoloLearn-Unity/Assets/Scripts/GravityController.cs
@@ -0,0 +1,29 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private CustomHandDraggable hd;
+
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+        hd = gameObject.GetComponent<CustomHandDraggable>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (hd.isDragging)
+        {
+            rb.useGravity = false;
+        }
+
+        if (!hd.isDragging)
+        {
+            rb.useGravity = true;
+        }
+    }
+}
