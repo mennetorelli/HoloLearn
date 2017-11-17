928d0257cf710fa52dc752082e58fcf7f6a88ce5
Emanuele Torelli <mennetorelli@hotmail.it>
2017-11-17 10:46:41 +0100

    bugfix

--- HoloLearn-Unity/Assets/Scripts/CanController.cs
+++ HoloLearn-Unity/Assets/Scripts/CanController.cs
@@ -1,33 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CanController : MonoBehaviour {
-    private Rigidbody rb;
-	// Use this for initialization
-	void Start ()
-    {
-        rb = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-        rb.AddForce(movement);
-	}
 
     void OnTriggerEnter(Collider other)
     {
-        
-        if (other.gameObject.CompareTag("Cylinder"))
-       
+        if (gameObject.CompareTag(other.tag))
         {
-            gameObject.SetActive(false);
+            other.gameObject.SetActive(false);
         }        
     } 
 }
--- HoloLearn-Unity/Assets/Scripts/PlacementController.cs
+++ HoloLearn-Unity/Assets/Scripts/PlacementController.cs
@@ -7,7 +7,7 @@ public class PlacementController : MonoBehaviour {
         Debug.Log(gameObject.tag + other.tag);
         if (other.gameObject.CompareTag(gameObject.tag))
         {
-            Vector3 newPos = new Vector3(other.gameObject.transform.position.x, other.gameObject.transform.position.y, other.gameObject.transform.position.z);
+            Vector3 newPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
             other.gameObject.transform.position = newPos;
 
             gameObject.SetActive(false);
