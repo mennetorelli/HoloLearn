bb7f5314fe45b0ad84a9bd97520d3e5bdaae3e2c
Emanuele Torelli <mennetorelli@hotmail.it>
2017-11-17 09:21:07 +0100

    lay the table WIP

---
+++ HoloLearn-Unity/Assets/Scripts/PlacementController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlacementController : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(gameObject.tag + other.tag);
+        if (other.gameObject.CompareTag(gameObject.tag))
+        {
+            Vector3 newPos = new Vector3(other.gameObject.transform.position.x, other.gameObject.transform.position.y, other.gameObject.transform.position.z);
+            other.gameObject.transform.position = newPos;
+
+            gameObject.SetActive(false);
+        }
+    }
+}
