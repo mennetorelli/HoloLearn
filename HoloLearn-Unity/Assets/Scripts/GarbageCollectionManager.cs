f95a99152ef5d5dad7932589caa88afe2104e0c6
Emanuele Torelli <mennetorelli@hotmail.it>
2017-11-18 18:18:06 +0100

    aggiustati prefab garbage collection

---
+++ HoloLearn-Unity/Assets/Scripts/GarbageCollectionManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class GarbageCollectionManager : MonoBehaviour
+{
+
+    public static int GARBAGE_COLLECTION_LEVEL;
+    public GameObject garbageCollectionObjects;
+
+    // Use this for initialization
+    void Start()
+    {
+        GARBAGE_COLLECTION_LEVEL = 2;
+
+        //Per scegliere a seconda del livello
+        Transform selectedLevel = garbageCollectionObjects.transform.GetChild(GARBAGE_COLLECTION_LEVEL - 1);
+
+        //Da creare un metodo per posizionare gli oggetti
+        Transform bins = selectedLevel.transform.GetChild(0);
+        Instantiate(bins.gameObject, bins.position, bins.rotation);
+
+        //Da creare un metodo per posizionare gli oggetti
+        Transform waste = selectedLevel.transform.GetChild(1);
+        Instantiate(waste.gameObject, waste.position, waste.rotation);
+    }
+}
--- HoloLearn-Unity/Assets/Scripts/LayTheTableManager.cs
+++ HoloLearn-Unity/Assets/Scripts/LayTheTableManager.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class GameObjectsManager : MonoBehaviour {
+public class LayTheTableManager : MonoBehaviour {
 
-    public static int LEVEL_SETTINGS;
+    public static int LAY_THE_TABLE_LEVEL;
     public GameObject layTheTableObjects;
 
 	// Use this for initialization
 	void Start () {
-        LEVEL_SETTINGS = 2;
+        LAY_THE_TABLE_LEVEL = 1;
 
-        //Per scegliere il livello
-        Transform selectedLevel = layTheTableObjects.transform.GetChild(LEVEL_SETTINGS - 1);
+        //Per scegliere a seconda del livello
+        Transform selectedLevel = layTheTableObjects.transform.GetChild(LAY_THE_TABLE_LEVEL - 1);
 
-        //Da mandare ad un metodo apposito
+        //Da creare un metodo per posizionare gli oggetti
         Transform objectsToBePlaced = selectedLevel.transform.GetChild(0);
         Instantiate(objectsToBePlaced.gameObject, objectsToBePlaced.position, objectsToBePlaced.rotation);
 
-        //Da mandare ad un metodo apposito
+        //Da creare un metodo per posizionare gli oggetti
         Transform placements = selectedLevel.transform.GetChild(1);
         Instantiate(placements.gameObject, placements.position, placements.rotation);
      }
