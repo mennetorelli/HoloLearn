1dba4b54d1d291c96a5603dd5cb8e045af3885f7
HglNgg <raggafra@gmail.com>
2017-11-25 17:08:19 +0100

    intro quasi completata

--- HoloLearn-Unity/Assets/Game Jam Template/Scripts/DontDestroy.cs
+++ HoloLearn-Unity/Assets/Game Jam Template/Scripts/DontDestroy.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class DontDestroy : MonoBehaviour {
 
+    public int players;
+    public int level;
 	void Start()
 	{
 		//Causes UI object not to be destroyed when loading a new scene. If you want it to be destroyed, destroy it manually via script.
---
+++ HoloLearn-Unity/Assets/Game Jam Template/Scripts/SetSettings.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class SetSettings : MonoBehaviour
+{
+    public int level;
+    public int people;
+
+   public void setLevelToEasy()
+    {
+        level = 1;
+    }
+
+    public void setLevelToHard()
+    {
+        level = 2;
+    }
+    public void setPeopleOne()
+    {
+        people = 1;
+    }
+    public void setPeopleTwo()
+    {
+        people = 2;
+    }
+    public void setPeopleThree()
+    {
+        people = 3;
+    }
+}
