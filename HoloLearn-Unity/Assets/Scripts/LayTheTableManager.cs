24e3185fe82515126657a3ed5d69bd8f37ea085f
Emanuele Torelli <mennetorelli@hotmail.it>
2017-11-25 13:52:52 +0100

    posizionamento oggetti WIP

--- HoloLearn-Unity/Assets/Scripts/LayTheTableManager.cs
+++ HoloLearn-Unity/Assets/Scripts/LayTheTableManager.cs
@@ -29,19 +29,21 @@ public class LayTheTableManager : Singleton<LayTheTableManager>
         //Da creare un metodo per posizionare gli oggetti
         Transform objectsToBePlaced = layTheTableObjects.transform.GetChild(0);
 
+        Vector3 tableCorner = new Vector3(0.0f, 0.0f, 4.0f);
+
         System.Random rnd = new System.Random();
 
         Transform plates = objectsToBePlaced.Find("Plates");
         for (int i=0; i<numberOfPeople; i++)
         {
-            Instantiate(plates.gameObject, plates.transform.position + new Vector3(0.0f, 0.1f, 0.0f), plates.transform.rotation);
+            Instantiate(plates.gameObject, tableCorner + new Vector3(0.0f, 0.1f, 0.0f), plates.transform.rotation);
         }
         
         Transform glasses = objectsToBePlaced.Find("Glasses");
         Transform glassType = glasses.GetChild(rnd.Next(0, glasses.childCount - 1));
         for (int i = 0; i < numberOfPeople; i++)
         {
-            Instantiate(glassType.gameObject, glassType.transform.position + new Vector3(0.0f, 0.1f, 0.0f), glassType.transform.rotation);
+            Instantiate(glassType.gameObject, tableCorner + new Vector3(0.1f, 0.1f, 0.0f), glassType.transform.rotation);
         }
         
         Transform cutlery = objectsToBePlaced.Find("Cutlery");
@@ -49,15 +51,15 @@ public class LayTheTableManager : Singleton<LayTheTableManager>
         Transform cutleryType2 = cutlery.GetChild(rnd.Next(1, 3));
         for (int i = 0; i < numberOfPeople; i++)
         {
-            Instantiate(cutleryType1.gameObject, cutleryType1.transform.position + new Vector3(0.0f, 0.1f, 0.0f), cutleryType1.transform.rotation);
-            Instantiate(cutleryType2.gameObject, cutleryType2.transform.position + new Vector3(0.0f, 0.1f, 0.0f), cutleryType2.transform.rotation);
+            Instantiate(cutleryType1.gameObject, tableCorner + new Vector3(-0.3f, 0.0f, 0.0f), cutleryType1.transform.rotation);
+            Instantiate(cutleryType2.gameObject, tableCorner + new Vector3(-0.4f, 0.2f, 0.0f), cutleryType2.transform.rotation);
         }
         
         Transform beverages = objectsToBePlaced.Find("Beverages");
         Transform bottle = beverages.Find("WaterBottle");
-        Instantiate(bottle.gameObject, bottle.transform.position + new Vector3(0.0f, 0.1f, 0.0f), bottle.transform.rotation);
+        Instantiate(bottle.gameObject, tableCorner + new Vector3(-0.1f, 0.1f, 0.2f), bottle.transform.rotation);
         Transform can = beverages.GetChild(rnd.Next(1, 3));
-        Instantiate(can.gameObject, can.transform.position + new Vector3(0.0f, 0.1f, 0.0f), can.transform.rotation);
+        Instantiate(can.gameObject, tableCorner + new Vector3(-0.1f, 0.1f, 0.2f), can.transform.rotation);
 
 
 
