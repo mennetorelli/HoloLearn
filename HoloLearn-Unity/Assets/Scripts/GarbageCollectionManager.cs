d0f7712a0ac8b68efd82063758eba86b4407b054
Emanuele Torelli <mennetorelli@hotmail.it>
2017-11-27 12:09:35 +0100

    iniziata dispostizione oggetti garbage collection

--- HoloLearn-Unity/Assets/Scripts/GarbageCollectionManager.cs
+++ HoloLearn-Unity/Assets/Scripts/GarbageCollectionManager.cs
@@ -1,29 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-
+using System.Linq;
 using UnityEngine;
 
 public class GarbageCollectionManager : MonoBehaviour
 {
 
-    public static int GARBAGE_COLLECTION_LEVEL;
-    public GameObject garbageCollectionObjects;
+    public GameObject BinsPrefabs;
+    public GameObject WastePrefabs;
+
+    private int numberOfBins;
+    private int numberOfObjects;
 
     // Use this for initialization
     void Start()
     {
-        GARBAGE_COLLECTION_LEVEL = 2;
+        numberOfBins = 10;
+        numberOfObjects = 4;
 
-        //Per scegliere a seconda del livello
-        Transform selectedLevel = garbageCollectionObjects.transform.GetChild(GARBAGE_COLLECTION_LEVEL - 1);
+        GenerateObjectsInWorld(null, null);
+    }
 
-        //Da creare un metodo per posizionare gli oggetti
-        Transform bins = selectedLevel.transform.GetChild(0);
-        Instantiate(bins.gameObject, bins.position, bins.rotation);
+    internal void GenerateObjectsInWorld(List<GameObject> floors, List<GameObject> tables)
+    {
+        //Seleziono il pavimento
+        //Transform floor = floors.ElementAt(0).transform;
 
-        //Da creare un metodo per posizionare gli oggetti
-        Transform waste = selectedLevel.transform.GetChild(1);
-        Instantiate(waste.gameObject, waste.position, waste.rotation);
+        Transform bins = new GameObject("Bins").transform;
+
+        System.Random rnd = new System.Random();
+
+
+        //DA TOGLIERE IL BIDONE SE è GIà STATO MESSO
+        for (int i=0; i<numberOfBins; i++)
+        {
+            Transform bin = BinsPrefabs.transform.GetChild(rnd.Next(0, BinsPrefabs.transform.childCount));
+            Instantiate(bin, new Vector3((float)Math.Pow(-1, i) * 0.4f * (i/2), 0f, 0f), bin.rotation, bins);
+        }
+    
     }
 }
