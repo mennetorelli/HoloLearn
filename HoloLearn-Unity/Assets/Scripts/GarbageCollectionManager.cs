e7afb680eb6a509b14aeae02d36ddc9a30c85541
Emanuele Torelli <mennetorelli@hotmail.it>
2017-11-27 16:34:05 +0100

    bidoni giusti

--- HoloLearn-Unity/Assets/Scripts/GarbageCollectionManager.cs
+++ HoloLearn-Unity/Assets/Scripts/GarbageCollectionManager.cs
@@ -13,10 +13,12 @@ public class GarbageCollectionManager : MonoBehaviour
     private int numberOfBins;
     private int numberOfObjects;
 
+    private List<string> activeBins = new List<string>();
+
     // Use this for initialization
     void Start()
     {
-        numberOfBins = 10;
+        numberOfBins = 3;
         numberOfObjects = 4;
 
         GenerateObjectsInWorld(null, null);
@@ -33,10 +35,24 @@ public class GarbageCollectionManager : MonoBehaviour
 
 
         //DA TOGLIERE IL BIDONE SE è GIà STATO MESSO
-        for (int i=0; i<numberOfBins; i++)
+        for (int i=1; i<=numberOfBins;)
         {
             Transform bin = BinsPrefabs.transform.GetChild(rnd.Next(0, BinsPrefabs.transform.childCount));
-            Instantiate(bin, new Vector3((float)Math.Pow(-1, i) * 0.4f * (i/2), 0f, 0f), bin.rotation, bins);
+            String currentBinTag = bin.gameObject.tag;
+            Debug.Log(currentBinTag);
+            if (!activeBins.Contains(currentBinTag))
+            {
+                Instantiate(bin, new Vector3((float)Math.Pow(-1, i) * 0.4f * (i / 2), 0f, 4f), bin.rotation, bins);
+                activeBins.Add(bin.gameObject.tag);
+                i++;
+            }
+        }
+
+
+        for (int i=0; i<numberOfObjects; i++)
+        {
+            Transform waste = WastePrefabs.transform.GetChild(rnd.Next(0, WastePrefabs.transform.childCount));
+            Instantiate(waste, new Vector3(0f, 0f, 1f), waste.rotation);
         }
     
     }
