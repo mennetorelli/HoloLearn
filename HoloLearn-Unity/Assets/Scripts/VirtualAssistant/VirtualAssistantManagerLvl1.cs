c95dff9b9805e54165fdba88a64a11ca1369ef04
Emanuele Torelli <mennetorelli@hotmail.it>
2017-12-07 13:15:29 +0100

    fix rotazione minion

--- HoloLearn-Unity/Assets/Scripts/VirtualAssistant/VirtualAssistantManagerLvl1.cs
+++ HoloLearn-Unity/Assets/Scripts/VirtualAssistant/VirtualAssistantManagerLvl1.cs
@@ -17,7 +17,12 @@ namespace Assets.Scripts.VirtualAssistant
         // Update is called once per frame
         public override void Update()
         {
-            gameObject.transform.LookAt(Camera.main.transform.position);
+            Vector3 relativePos = Camera.main.transform.position - gameObject.transform.position;
+            Quaternion rotation = Quaternion.LookRotation(relativePos);
+            rotation.x = 0f;
+            rotation.z = 0f;
+
+            gameObject.transform.rotation = rotation;
         }
 
         public override void Jump()
