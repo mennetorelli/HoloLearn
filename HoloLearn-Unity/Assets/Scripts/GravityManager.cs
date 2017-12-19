bd2497a2a96c622b7390fb2c00b26f3a9b8d7fca
Emanuele Torelli <mennetorelli@hotmail.it>
2017-12-19 19:33:24 +0100

    ...

--- HoloLearn-Unity/Assets/Scripts/GravityManager.cs
+++ HoloLearn-Unity/Assets/Scripts/GravityManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GravityController : MonoBehaviour {
+public class GravityManager : MonoBehaviour {
 
     private Rigidbody rb;
     private CustomHandDraggable hd;
