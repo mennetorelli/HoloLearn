c09bafe6e18ab1ee5d15507432bbb9ecc3e1e0fc
Emanuele Torelli <mennetorelli@hotmail.it>
2018-01-05 00:02:57 +0100

    rotazione cutlery

---
+++ HoloLearn-Unity/Assets/Scripts/LayTheTable/ObjectsPositionManagers/CutleryPositionManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutleryPositionManager : ObjectPositionManager
+{
+    
+    public override void AdjustTransform()
+    {
+        finalRotation.x = 180f;
+    }
+}
