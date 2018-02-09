f450f2c2f7e22b5be6d438c2f6c1db27757942c0
Emanuele Torelli <mennetorelli@hotmail.it>
2018-02-10 00:02:53 +0100

    speech handler WIP

---
+++ HoloLearn-Unity/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,36 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour, ISpeechHandler
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void OnSpeechKeywordRecognized(SpeechEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+    public void SetMenuVisible()
+    {
+        transform.GetChild(1).gameObject.SetActive(true);
+    }
+
+    public void StartPlay()
+    {
+
+    }
+
+}
