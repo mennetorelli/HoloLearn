0cb97fd0d99945159c1f8683e18fa49db50b6bfd
Emanuele Torelli <mennetorelli@hotmail.it>
2018-02-11 17:41:32 +0100

    da eliminare tutti i singleton

--- HoloLearn-Unity/Assets/Scripts/Menu/StartOptions.cs
+++ HoloLearn-Unity/Assets/Scripts/Menu/StartOptions.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using HoloToolkit.Unity;
 
 namespace HoloLearn
 {
@@ -9,6 +10,14 @@ namespace HoloLearn
     {
         public void ChangeScene(int scene)
         {
+            if (scene == 0)
+            {
+                foreach (GameObject item in SceneManager.GetActiveScene().GetRootGameObjects())
+                {
+                    Debug.Log(item);
+                    // eliminare tutti i singleton
+                }
+            }
             SceneManager.LoadScene(scene);
         }
     }
