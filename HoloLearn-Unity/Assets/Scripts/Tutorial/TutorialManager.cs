3948c1554013e9cd4b2c9e9abace215783e81313
Emanuele Torelli <mennetorelli@hotmail.it>
2018-10-26 17:43:11 +0200

    iniziata scena tutorial

---
+++ HoloLearn-Unity/Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,105 @@
+﻿using HoloToolkit.Unity;
+using HoloToolkit.Unity.SpatialMapping;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class TutorialManager : TaskManager
+{
+
+    public GameObject WeatherPrefabs;
+    public GameObject ClothesPrefabs;
+    public GameObject VirtualAssistantsPrefabs;
+
+    private int assistantPresence;
+    private int selectedAssistant;
+    private int assistantBehaviour;
+    private int assistantPatience;
+
+    private Transform virtualAssistant;
+
+
+    // Use this for initialization
+    public override void Start()
+    {
+        LoadSettings();
+
+        virtualAssistant = VirtualAssistantsPrefabs.transform.GetChild(selectedAssistant + 1).GetChild(assistantBehaviour - 1);
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+
+    }
+
+    public override void GenerateObjectsInWorld()
+    {
+        //Seleziono il pavimento
+        Transform floor = FloorProcessing.Instance.floors.ElementAt(0).transform;
+        SurfacePlane plane = floor.GetComponent<SurfacePlane>();
+
+        System.Random rnd = new System.Random();
+
+
+
+        Vector3 floorPosition = floor.transform.position + (plane.PlaneThickness * plane.SurfaceNormal);
+        floorPosition = AdjustPositionWithSpatialMap(floorPosition, plane.SurfaceNormal);
+
+        Vector3 gazePosition = new Vector3(0f, 0f, 0f);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, 20f, Physics.DefaultRaycastLayers))
+        {
+            gazePosition = hitInfo.point;
+        }
+
+
+
+
+        Vector3 assistantPosition = gazePosition;
+        assistantPosition.y = floor.position.y;
+
+        if (assistantPresence != 0)
+        {
+            Instantiate(virtualAssistant.gameObject, assistantPosition, virtualAssistant.transform.rotation);
+            VirtualAssistantManager.Instance.patience = assistantPatience;
+            VirtualAssistantManager.Instance.transform.localScale += new Vector3(0.25f * VirtualAssistantManager.Instance.transform.localScale.x, 0.25f * VirtualAssistantManager.Instance.transform.localScale.y, 0.25f * VirtualAssistantManager.Instance.transform.localScale.z);
+        }
+
+    }
+
+
+    /// <summary>
+    /// Adjusts the initial position of the object if it is being occluded by the spatial map.
+    /// </summary>
+    /// <param name="position">Position of object to adjust.</param>
+    /// <param name="surfaceNormal">Normal of surface that the object is positioned against.</param>
+    /// <returns></returns>
+    private Vector3 AdjustPositionWithSpatialMap(Vector3 position, Vector3 surfaceNormal)
+    {
+        Vector3 newPosition = position;
+        RaycastHit hitInfo;
+        float distance = 0.5f;
+
+        // Check to see if there is a SpatialMapping mesh occluding the object at its current position.
+        if (Physics.Raycast(position, surfaceNormal, out hitInfo, distance, SpatialMappingManager.Instance.LayerMask))
+        {
+            // If the object is occluded, reset its position.
+            newPosition = hitInfo.point;
+        }
+
+        return newPosition;
+    }
+
+
+    private void LoadSettings()
+    {
+        assistantPresence = VirtualAssistantChoice.Instance.assistantPresence;
+        selectedAssistant = VirtualAssistantChoice.Instance.selectedAssistant;
+        assistantBehaviour = VirtualAssistantSettings.Instance.assistantBehaviour;
+        assistantPatience = VirtualAssistantSettings.Instance.assistantPatience;
+    }
+}
