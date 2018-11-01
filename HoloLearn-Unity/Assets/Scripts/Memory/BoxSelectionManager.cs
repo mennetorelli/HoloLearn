dea37a029eac63c71772fcea80ad4a1b4b2bf8fe
Emanuele Torelli <mennetorelli@hotmail.it>
2018-11-01 19:41:48 +0100

    memory WIP

--- HoloLearn-Unity/Assets/Scripts/Memory/BoxSelectionManager.cs
+++ HoloLearn-Unity/Assets/Scripts/Memory/BoxSelectionManager.cs
@@ -22,23 +22,35 @@ public class BoxSelectionManager : MonoBehaviour, IInputClickHandler
         transform.parent.GetChild(1).gameObject.SetActive(true);
 
         MemoryManager manager = (MemoryManager)TaskManager.Instance;
-        if (manager.selectedObject != null)
+        if (manager.selectedElement != null)
         {
-            if (manager.selectedObject.name == transform.parent.GetChild(1).gameObject.name)
+            if (manager.selectedElement.transform.GetChild(1).gameObject.name == transform.parent.GetChild(1).gameObject.name)
             {
                 VirtualAssistantManager.Instance.Jump();
             }
             else
             {
                 VirtualAssistantManager.Instance.ShakeHead();
+                Debug.Log("da aspettare");
+                StartCoroutine(Wait());
+                transform.parent.GetChild(0).gameObject.SetActive(true);
+                transform.parent.GetChild(1).gameObject.SetActive(false);
+                manager.selectedElement.transform.GetChild(0).gameObject.SetActive(true);
+                manager.selectedElement.transform.GetChild(1).gameObject.SetActive(false);
+                Debug.Log("ritornato");
             }
-            manager.selectedObject = null;
+            manager.selectedElement = null;
         }
         else
         {
-            manager.selectedObject = transform.parent.GetChild(1).gameObject;
-            Debug.Log(manager.selectedObject);
+            manager.selectedElement = transform.parent.gameObject;
         }
         
+    }
+
+
+    public IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(3f);
     }
 }
--- HoloLearn-Unity/Assets/Scripts/Memory/MemoryManager.cs
+++ HoloLearn-Unity/Assets/Scripts/Memory/MemoryManager.cs
@@ -23,7 +23,7 @@ public class MemoryManager : TaskManager
 
     private Transform virtualAssistant;
 
-    public GameObject selectedObject; 
+    public GameObject selectedElement; 
 
     // Use this for initialization
     public override void Start()
@@ -85,7 +85,8 @@ public class MemoryManager : TaskManager
             elem.parent = elems;
             GameObject box = Instantiate(BoxPrefab, new Vector3((float)Math.Pow(-1, i) * 0.4f * (i / 2), 0f, 0f), BoxPrefab.transform.rotation, elem);
             int j = rnd.Next(0, objs.Count);
-            Instantiate(objs.ElementAt(j), box.transform.position, box.transform.rotation, elem);
+            Transform obj = Instantiate(objs.ElementAt(j), box.transform.position, box.transform.rotation, elem);
+            obj.gameObject.SetActive(false);
             objs.RemoveAt(j);
         }
 
@@ -139,4 +140,5 @@ public class MemoryManager : TaskManager
         assistantPresence = VirtualAssistantChoice.Instance.assistantPresence;
         selectedAssistant = VirtualAssistantChoice.Instance.selectedAssistant;
     }
+
 }
