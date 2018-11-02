3deb9b2c35534b467738be5bf8831423ebe54fe8
Emanuele Torelli <mennetorelli@hotmail.it>
2018-11-02 14:42:51 +0100

    temperatura generata dinamicamente

---
+++ HoloLearn-Unity/Assets/Scripts/DressUp/TemperatureGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemperatureGenerator : MonoBehaviour {
+
+    public int MinRange;
+    public int MaxRange;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<TextMesh>().text = new System.Random().Next(MinRange, MaxRange).ToString() + "°C";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
