4df6d251a2c47831389869d3851e58d801dc7826
Emanuele Torelli <mennetorelli@hotmail.it>
2019-02-20 16:50:41 +0100

    Update BallPositionManager.cs

--- HoloLearn-Unity/Assets/Scripts/Tutorial/BallPositionManager.cs
+++ HoloLearn-Unity/Assets/Scripts/Tutorial/BallPositionManager.cs
@@ -7,14 +7,15 @@ using UnityEngine;
 public class BallPositionManager : ObjectPositionManager
 {
     private bool hasCollided;
+    private bool lerpDone;
     private Vector3 targetPosition;
+    Vector3 floorPosition;
 
     public override void HasCollided(Transform target)
     {
         transform.GetComponentInChildren<MeshCollider>().enabled = false;
 
         targetPosition = target.TransformPoint(target.GetComponentInChildren<BoxCollider>().center);
-        transform.position = targetPosition;
 
         hasCollided = true;
     }
@@ -23,19 +24,36 @@ public class BallPositionManager : ObjectPositionManager
     public override void Start()
     {
         hasCollided = false;
+        lerpDone = false;
         targetPosition = new Vector3();
+        floorPosition = GameObject.Find("SurfacePlane(Clone)").transform.position;
     }
 
     public override void Update()
     {
+        if (transform.position.y < floorPosition.y)
+        {
+            transform.position = new Vector3(transform.position.x, floorPosition.y + 0.01f, transform.position.z);
+        }
+
         if (hasCollided)
         {
-            Vector3 floorPosition = GameObject.Find("SurfacePlane(Clone)").transform.position;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 5f);
+            if (Vector3.Distance(transform.position, targetPosition) < 0.01f)
+            {
+                lerpDone = true;
+                hasCollided = false;
+            }
+        }
+
+        if (lerpDone)
+        {
+            
             if (Math.Abs(transform.position.y - floorPosition.y) < 0.1)
             {
                 transform.GetComponentInChildren<MeshCollider>().enabled = true;
                 transform.GetComponent<CustomHandDraggable>().IsDraggingEnabled = true;
-                hasCollided = false;
+                lerpDone = false;
             }
         }
     }
