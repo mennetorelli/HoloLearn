85585df2ab4cf7ed8d6fa6913234593657eb73d7
Emanuele Torelli <mennetorelli@hotmail.it>
2019-04-08 16:56:03 +0200

    iniziata nuova logica ManipulationHandler

---
+++ HoloLearn-Unity/Assets/Scripts/ManipulationEventHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManipulationEventHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void ManipulationStarted(GameObject draggedObject)
+    {
+        if (VirtualAssistantManager.Instance != null)
+        {
+            VirtualAssistantManager.Instance.ObjectDragged(draggedObject);
+        }
+
+        if (PlacementsManager.Instance != null)
+        {
+            PlacementsManager.Instance.MakeLevelPrefabDisappear(draggedObject);
+        }
+    }
+
+    public void ManipulationStopped()
+    {
+        if (VirtualAssistantManager.Instance != null)
+        {
+            VirtualAssistantManager.Instance.ObjectDropped();
+        }
+
+        if (PlacementsManager.Instance != null)
+        {
+            PlacementsManager.Instance.MakeLevelPrefabReapper();
+        }
+    }
+}
