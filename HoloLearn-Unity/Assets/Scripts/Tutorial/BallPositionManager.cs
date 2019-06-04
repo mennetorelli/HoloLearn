72bbb2877f7b91be579d1800c9ba764e24a35d59
Emanuele Torelli <mennetorelli@hotmail.it>
2019-06-04 12:44:22 +0200

    Update BallPositionManager.cs

--- HoloLearn-Unity/Assets/Scripts/Tutorial/BallPositionManager.cs
+++ HoloLearn-Unity/Assets/Scripts/Tutorial/BallPositionManager.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 public class BallPositionManager : ObjectPositionManager
 {
     private bool hasCollided;
-    private bool lerpDone;
     private Vector3 targetPosition;
     private Vector3 floorPosition;
 
@@ -15,7 +14,6 @@ public class BallPositionManager : ObjectPositionManager
     public override void Start()
     {
         hasCollided = false;
-        lerpDone = false;
         targetPosition = new Vector3();
         floorPosition = GameObject.Find("SurfacePlane(Clone)").transform.position;
     }
@@ -27,7 +25,7 @@ public class BallPositionManager : ObjectPositionManager
             transform.position = new Vector3(transform.position.x, floorPosition.y + 0.01f, transform.position.z);
         }
 
-        if (!hasCollided && !lerpDone && !transform.GetComponent<Collider>().isTrigger && Math.Abs(transform.position.y - floorPosition.y) > 0.2f)
+        if (!hasCollided && !transform.GetComponent<Collider>().isTrigger && Math.Abs(transform.position.y - floorPosition.y) > 0.2f)
         {
             transform.GetComponent<Collider>().isTrigger = true;
         }
@@ -35,21 +33,11 @@ public class BallPositionManager : ObjectPositionManager
         if (hasCollided)
         {
             transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 5f);
-            if (Vector3.Distance(transform.position, targetPosition) < 0.01f)
+            if (Vector3.Distance(transform.position, targetPosition) < 0.01f || Math.Abs(transform.position.y - floorPosition.y) < 0.2f)
             {
-                lerpDone = true;
                 hasCollided = false;
                 transform.GetComponent<Rigidbody>().isKinematic = false;
-            }
-        }
-
-        if (lerpDone)
-        {
-            if (Math.Abs(transform.position.y - floorPosition.y) < 0.2f)
-            {
                 transform.GetComponentInChildren<Collider>().enabled = true;
-                transform.GetComponent<ManipulationHandler>().enabled = true;
-                lerpDone = false;
             }
         }
     }
@@ -57,7 +45,7 @@ public class BallPositionManager : ObjectPositionManager
 
     public override void HasCollided(Transform target)
     {
-        transform.GetComponent<ManipulationHandler>().enabled = false;
+        transform.GetComponent<ManipulationEventHandler>().ManipulationStopped();
         transform.GetComponent<Collider>().enabled = false;
         transform.GetComponent<Rigidbody>().isKinematic = true;
 
